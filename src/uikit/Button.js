d28ada46c202a5278f20209d2b8e0bf5056d39e0
alex-alk <alex.leo06@yahoo.ro>
2025-09-22 22:34:34 +0300

    update

--- src/uikit/Button.js
+++ src/uikit/Button.js
@@ -9,11 +9,39 @@ export class Button {
         <div class="card flex flex-col gap-4">
             <div class="font-semibold text-xl">Default</div>
             <div class="flex flex-wrap gap-2">
-                <button class="p-button p-component" type="button">
-                    <span class="font-500">Submit</span>
+                <button class="btn" type="button">
+                    <span class="btn-label">Submit</span>
+                </button>
+                <button class="btn" type="button" disabled>
+                    <span class="btn-label">Disabled</span>
+                </button>
+                <button class="btn btn-link" type="button">
+                    <span class="btn-label">Link</span>
                 </button>
             </div>
         </div>
+
+        <div class="card flex flex-col gap-4">
+            <div class="font-semibold text-xl">Severities</div>
+            <div class="flex flex-wrap gap-2">
+                <button class="btn" type="button">
+                    <span class="btn-label">Primary</span>
+                </button>
+                <button class="btn btn-secondary" type="button">
+                    <span class="btn-label">Secondary</span>
+                </button>
+                <button class="btn btn-success" type="button">
+                    <span class="btn-label">Success</span>
+                </button>
+                <button class="btn btn-info" type="button">
+                    <span class="btn-label">Info</span>
+                </button>
+            </div>
+        </div>
+
+    </div>
+    <div class="md:w-1/2">
+
     </div>
 </div>`
         const component = createComponent(html)
