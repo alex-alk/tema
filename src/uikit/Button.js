02148347d7d199562ae84d0cf75ae3312d7696cd
alex-alk <alex.leo06@yahoo.ro>
2025-09-23 21:31:08 +0300

    update

--- src/uikit/Button.js
+++ src/uikit/Button.js
@@ -36,6 +36,48 @@ export class Button {
                 <button class="btn btn-info" type="button">
                     <span class="btn-label">Info</span>
                 </button>
+                <button class="btn btn-warn" type="button">
+                    <span class="btn-label">Warn</span>
+                </button>
+                <button class="btn btn-help" type="button">
+                    <span class="btn-label">Help</span>
+                </button>
+                <button class="btn btn-danger" type="button">
+                    <span class="btn-label">Danger</span>
+                </button>
+                <button class="btn btn-contrast" type="button">
+                    <span class="btn-label">Contrast</span>
+                </button>
+            </div>
+        </div>
+
+        <div class="card flex flex-col gap-4">
+            <div class="font-semibold text-xl">Text</div>
+            <div class="flex flex-wrap gap-2">
+                <button class="btn btn-text" type="button">
+                    <span class="btn-label">Primary</span>
+                </button>
+                <button class="btn btn-text-secondary" type="button">
+                    <span class="btn-label">Secondary</span>
+                </button>
+                <button class="btn btn-text-success" type="button">
+                    <span class="btn-label">Success</span>
+                </button>
+                <button class="btn btn-text-info" type="button">
+                    <span class="btn-label">Info</span>
+                </button>
+                <button class="btn btn-text-warn" style="color" type="button">
+                    <span class="btn-label">Warn</span>
+                </button>
+                <button class="btn btn-text-help" type="button">
+                    <span class="btn-label">Help</span>
+                </button>
+                <button class="btn btn-text-danger" type="button">
+                    <span class="btn-label">Danger</span>
+                </button>
+                <button class="btn btn-text-contrast" type="button">
+                    <span class="btn-label">Contrast</span>
+                </button>
             </div>
         </div>
 
