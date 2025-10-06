b19283db6428d940eb0ebfa7df547b4d6bb065a2
alex-alk <alex.leo06@yahoo.ro>
2025-10-06 23:02:49 +0300

    update

--- src/uikit/Button.js
+++ src/uikit/Button.js
@@ -135,13 +135,122 @@ export class Button {
             <div class="font-semibold text-xl">Split Button</div>
             <div class="flex flex-wrap gap-2">
                 <div class="btnsplit">
-                    <button class="btn btn-outline-danger" type="button">
-                        <span class="btn-label">Danger</span>
+                    <button class="btn btn-split" type="button">
+                        <span class="btn-label">Save</span>
                     </button>
-                    <button class="btn btn-outline-danger" type="button">
-                        <span class="btn-label">Danger</span>
+                    <button class="btn btn-split-dropdown" type="button">
+                        <svg 
+                            width="14" 
+                            height="14" 
+                            viewBox="0 0 14 14" 
+                            fill="none" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            class="icon">
+                            <path d="M7.01744 10.398C6.91269 10.3985 6.8089 10.378 6.71215 10.3379C6.61541 10.2977 6.52766 10.2386 6.45405 10.1641L1.13907 4.84913C1.03306 4.69404 0.985221 4.5065 1.00399 4.31958C1.02276 4.13266 1.10693 3.95838 1.24166 3.82747C1.37639 3.69655 1.55301 3.61742 1.74039 3.60402C1.92777 3.59062 2.11386 3.64382 2.26584 3.75424L7.01744 8.47394L11.769 3.75424C11.9189 3.65709 12.097 3.61306 12.2748 3.62921C12.4527 3.64535 12.6199 3.72073 12.7498 3.84328C12.8797 3.96582 12.9647 4.12842 12.9912 4.30502C13.0177 4.48162 12.9841 4.662 12.8958 4.81724L7.58083 10.1322C7.50996 10.2125 7.42344 10.2775 7.32656 10.3232C7.22968 10.3689 7.12449 10.3944 7.01744 10.398Z" fill="currentColor"></path>
+                        </svg>
                     </button>
+                    <div class="tieredmenu">
+                        <ul class="tieredmenu-root-list">
+                            <li class="tieredmenu-item">
+                                <div class="tieredmenu-item-content">
+                                    <a class="tieredmenu-item-link">
+                                        <span class="tieredmenu-item-icon pi pi-refresh"></span>
+                                        <span class="leading-none">Update</span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li class="tieredmenu-item">
+                                <div class="tieredmenu-item-content">
+                                    <a class="tieredmenu-item-link">
+                                        <span class="tieredmenu-item-icon pi pi-times"></span>
+                                        <span class="leading-none">Delete</span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li class="tieredmenu-separator"></li>
+
+                            <li class="tieredmenu-item">
+                                <div class="tieredmenu-item-content">
+                                    <a class="tieredmenu-item-link">
+                                        <span class="tieredmenu-item-icon pi pi-home"></span>
+                                        <span class="leading-none">Home</span>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+
+                <div class="btnsplit">
+                    <button class="btn btn-secondary btn-split" type="button">
+                        <span class="btn-label">Save</span>
+                    </button>
+                    <button class="btn btn-secondary btn-split-dropdown" type="button">
+                        <svg 
+                            width="14" 
+                            height="14" 
+                            viewBox="0 0 14 14" 
+                            fill="none" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            class="icon">
+                            <path d="M7.01744 10.398C6.91269 10.3985 6.8089 10.378 6.71215 10.3379C6.61541 10.2977 6.52766 10.2386 6.45405 10.1641L1.13907 4.84913C1.03306 4.69404 0.985221 4.5065 1.00399 4.31958C1.02276 4.13266 1.10693 3.95838 1.24166 3.82747C1.37639 3.69655 1.55301 3.61742 1.74039 3.60402C1.92777 3.59062 2.11386 3.64382 2.26584 3.75424L7.01744 8.47394L11.769 3.75424C11.9189 3.65709 12.097 3.61306 12.2748 3.62921C12.4527 3.64535 12.6199 3.72073 12.7498 3.84328C12.8797 3.96582 12.9647 4.12842 12.9912 4.30502C13.0177 4.48162 12.9841 4.662 12.8958 4.81724L7.58083 10.1322C7.50996 10.2125 7.42344 10.2775 7.32656 10.3232C7.22968 10.3689 7.12449 10.3944 7.01744 10.398Z" fill="currentColor"></path>
+                        </svg>
+                    </button>
+                    <div class="tieredmenu">
+                        <ul class="tieredmenu-root-list">
+                            <li class="tieredmenu-item">
+                                <div class="tieredmenu-item-content">
+                                    <a class="tieredmenu-item-link">
+                                        <span class="tieredmenu-item-icon pi pi-refresh"></span>
+                                        <span class="leading-none">Update</span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li class="tieredmenu-item">
+                                <div class="tieredmenu-item-content">
+                                    <a class="tieredmenu-item-link">
+                                        <span class="tieredmenu-item-icon pi pi-times"></span>
+                                        <span class="leading-none">Delete</span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li class="tieredmenu-separator"></li>
+
+                            <li class="tieredmenu-item">
+                                <div class="tieredmenu-item-content">
+                                    <a class="tieredmenu-item-link">
+                                        <span class="tieredmenu-item-icon pi pi-home"></span>
+                                        <span class="leading-none">Home</span>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card flex flex-col gap-4">
+            <div class="font-semibold text-xl">Templating</div>
+            <div class="flex flex-wrap gap-2">
+                <span class="btngroup">
+                    <button class="btn" type="button">
+                        <span class="pi pi-check"></span>
+                        <span class="btn-label">Save</span>
+                    </button>
+                    <button class="btn" type="button">
+                        <span class="pi pi-trash"></span>
+                        <span class="btn-label">Delete</span>
+                    </button>
+                    <button class="btn" type="button">
+                        <span class="pi pi-times"></span>
+                        <span class="btn-label">Cancel</span>
+                    </button>
+                </span>
             </div>
         </div>
 
@@ -151,6 +260,24 @@ export class Button {
     </div>
 </div>`
         const component = createComponent(html)
+
+        const $btns = component.querySelectorAll('.btn-split-dropdown')
+
+        for (const $btn of $btns) {
+            const $menu = $btn.nextElementSibling;
+            $btn.addEventListener('click', 
+                (event) => {
+                    $menu.classList.toggle('tieredmenu-show')
+
+                    // if we do not use this, the click will be intercepted by document
+                    // and the menu will never show up
+                    event.stopPropagation()
+                }
+            );
+            document.addEventListener('click', function() {
+                $menu.classList.remove('tieredmenu-show')
+            })
+        }
         
         return component
     }
